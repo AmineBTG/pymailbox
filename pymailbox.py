752243023119cc67af35c09e69328b872d4f470a
AmineBTG <boutaghouamine@gmail.com>
2024-04-16 14:37:35 +0200

    import ABC

--- pymailbox.py
+++ pymailbox.py
@@ -1,5 +1,5 @@
 from src.exceptions import EmailNotFound, NoSearchResultsFound
 from src.search import EmailSearchCriteria
-from src.services import GmailEmailService, OutlookEmailService
+from src.services import EmailService, GmailEmailService, OutlookEmailService
 
 __all__ = ["GmailEmailService" "OutlookEmailService" "EmailNotFound" "NoSearchResultsFound" "EmailSearchCriteria"]
