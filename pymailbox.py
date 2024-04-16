a228856c08a4da0771e3a68e4a6623422ff86803
AmineBTG <boutaghouamine@gmail.com>
2024-04-16 14:59:06 +0200

    reorganized folder structure

--- pymailbox.py
+++
@@ -1,5 +0,0 @@
-from src.exceptions import EmailNotFound, NoSearchResultsFound
-from src.search import EmailSearchCriteria
-from src.services import EmailService, GmailEmailService, OutlookEmailService
-
-__all__ = ["GmailEmailService" "OutlookEmailService" "EmailNotFound" "NoSearchResultsFound" "EmailSearchCriteria"]
